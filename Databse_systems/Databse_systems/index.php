f9d4b851775184cbda06167eb7b7ae209c089684
Rami ALhussein <Ramialhussein98@gmail.com>
2019-04-22 00:41:30 -0400

    fixing the issue I had

--- Databse_systems/Databse_systems/index.php
+++ Databse_systems/Databse_systems/index.php
@@ -17,14 +17,8 @@ if(isset($_GET['logout'])){
 }
 
 $db = mysqli_connect('localhost', 'root', '', 'database_systems');
-$query = "SELECT * FROM products ORDER BY product_id ASC"
-
-//$username = $_SESSION['username'];
-//$query = "SELECT * FROM users where username='$username'";
-//$results = mysqli_query($db, $query);
-//$data = mysqli_fetch_assoc($results);
-
-$result = mysqli_query($db, $query)
+$query = "SELECT * FROM products ORDER BY product_id ASC";
+$result = mysqli_query($db, $query);
 
 ?>
 
@@ -36,14 +30,14 @@ $result = mysqli_query($db, $query)
         Rami's Shop | <?php echo $page_title ?>
     </title>
 
-    <?php include('./UserControls/topScripts.php') ?>
+    <?php include('./UserControls/topScripts.php'); ?>
 </head>
 <body>
-    <?php include('./UserControls/NavBar.php') ?>
-    <section class="container">
+    <?php include('./UserControls/NavBar.php'); ?>
+    <section>
         <section id="mainBody">
             <div class="row">
-                <div class="col-md-12" style="border:1px solid lightgray;">
+                <div class="col-md-12">
                     <div class="row">
                         <?php
                         if($result):
@@ -52,19 +46,19 @@ $result = mysqli_query($db, $query)
                         ?>
                         <div class="col-sm-4 col-md-3">
                             <form method="post" action="index.php?action=add&id=id=<?php $product['product_id']; ?>">
-                                <img src="<?php echo $product['img']; ?>" />
+                                <img src="<?php echo $product['img']; ?>" style="width: 18em;" />
                                 <label>
                                     <?php echo $product['name']; ?>
                                 </label>
                                 <label>
                                     Left in stock:  <?php
-                                                        if($product['quantity'] > 10){
-                                                           echo '<strong>10+</strong>';
-                                                        }else{
-                                                            echo $product['quantity'];
-                                                        }
+                                    if($product['quantity'] > 10){
+                                        echo '<strong>10+</strong>';
+                                    }else{
+                                        echo '<strong>'.$product['quantity'].'</strong>';
+                                    }
 
-                                    ?>
+                                                    ?>
                                 </label>
                                 <label>
                                     <?php echo $product['price']; ?>
@@ -72,16 +66,16 @@ $result = mysqli_query($db, $query)
                             </form>
                         </div>
                         <?php
-                                endwhile:
-                            endif:
-                        endif:
+                                endwhile;
+                            endif;
+                        endif;
                         ?>
                     </div>
                 </div>
             </div>
         </section>
     </section>
-    <?php include('./UserControls/bottomScripts.php') ?>
-    <?php include('./UserControls/Footer.php') ?>
+    <?php include('./UserControls/bottomScripts.php'); ?>
+    <?php include('./UserControls/Footer.php'); ?>
 </body>
 </html>
